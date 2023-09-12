2c4c28d880d91b67e2f2e934f4fc5ea66c5d17c7
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-12 14:03:52 +0200

    [edit] README

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Controllo Mail e Gioco dei Dadi
 1. Creo un input dove chiedo la mail dell'utente
 1. Creo una variabile per l'email dell'utente e una per il successivo messaggio di esito
 1. Verifico se l'email inserita dall'utente è presente nella lista delle email registate
-1. Se non è presente: non lo lascio entrare e stampo il relativo messaggio
 1. Se è presente: lo lascio entrare e stampo il relativo messaggio
+1. Se non è presente: non lo lascio entrare e stampo il relativo messaggio
 
 ## Gioco dei Dadi:
 1. Creo due variabili per il numero estratto dai due giocatori (numPlayer, numPC)
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-/*
-CONTROLLO MAIL
-*/
+/****************
+  CONTROLLO MAIL
+****************/
 // 1. Creo un array(lista) delle email registrate
 
 const emailRegistered = ['pippo', 'pluto', 'paperino'];
@@ -15,14 +15,28 @@ let emailUser, message;
 
 let emailVerify = false;
 
-emailUser = 'pippo'
+emailUser = 'pippo' //es. email inclusa in lista
 
 if (emailRegistered.includes(emailUser)) {
 
+  // 5. Se è presente: lo lascio entrare e stampo il relativo messaggio
   message = 'Puoi entrare';
   emailVerify = true;
+
 } else {
+
+  // 6. Se non è presente: non lo lascio entrare e stampo il relativo messaggio
   message = 'Non sei stato invitato';
+  
 }
 
 console.log(message)
+
+
+console.log('-------- DIVISIONE DA RIMUOVERE --------')
+
+
+/****************
+  GIOCO DADI
+****************/
+
